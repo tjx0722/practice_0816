61434cd32cef47b22efc0818ae3c292ae57a2251
tjx <443016895@qq.com>
2019-08-16 19:01:53 +0800

    搭建环境2019年8月16日19:01:50

---
+++ src/main/java/com/woniu/Practice0816Application.java
@@ -0,0 +1,13 @@
+package com.woniu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice0816Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practice0816Application.class, args);
+	}
+
+}
